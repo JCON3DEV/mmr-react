cf55f09c1cda4268d8f1d322414be3628560bab9
Jordan "Jay <43826746+JCON3DEV@users.noreply.github.com>
2020-09-24 15:33:29 -0700

    Initialize 2-clean-project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h4>Clean React App</h4>
+    </div>
+  );
+}
+
+export default App;
