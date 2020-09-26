cec1b5153b8f6a72dd40d9f07d158da748ce3efd
Jordan <jconheasman@gmail.com>
2020-09-26 13:22:45 -0700

    added bottom nav and table components to components folder as test using Material UI

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import axios from "axios";
 import Input from "./Input.js";
-import Button from "@material-ui/core/Button";
+import {Button, Typography} from "@material-ui/core";
+// import Box from "@material-ui/core/Box";
+import BasicTable from "./components/table";
+import BottomNav from "./components/bottom-navigation";
 import './App.css';
 
 export default function App() {
@@ -33,18 +36,25 @@ export default function App() {
   };
 
   return (
+    // <Container maxWidth="sm">
     <div className="App">
       <h3>{title}</h3>
       <h4>{url}</h4>
-
       {/* Use our new Text & Button Component */}
-      <Button variant="contained" color="primary">
+      <Button variant="outlined" color="secondary">
         Hello World
       </Button>
+      <Typography variant="h1" component="h2">
+        h1. Heading
+      </Typography>
       {/* Above button added for testing purposes */}
       <Input buttonText="Search" initial="React" onClick={searchClick} />
-
+      {/* <Box m={1}> */}
+      <BasicTable />
+      {/* <Box /> */}
+      <BottomNav />
       <ul>{hitsList}</ul>
     </div>
+    //{" "}
   );
 }
