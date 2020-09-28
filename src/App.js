01bcfa6b12327cdb9cc806e3bdbd9a0d3c20a145
Serah Sirohi <serahsirohi@gmail.com>
2020-09-28 22:12:58 +0000

    added router

--- src/App.js
+++ src/App.js
@@ -1,85 +1,36 @@
 import React, {useState, useEffect} from "react";
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 import axios from "axios";
-import Input from "./Input.js";
-import Homepage from "./components/homepage";
-import AboutUs from "./components/about-us";
-import YouTube from "./components/youtube";
-import "./App.css";
-
-
-
-// Christy's Components
-import PersistentDrawerRight from "./components/nav-drawer";
-
-//import Roboto Font
-import "fontsource-roboto";
+import Home from "./components/Home";
+import MammalProfile from "./components/MammalProfile";
+import LiveEvent from "./components/LiveEvent";
 
 export default function App() {
-  const title = "My Search Page";
-  const BASEURL = "/api/users/";
-  // above is example for test obj purposes
-  //https://hn.algolia.com/api/v1/search?query=';
-
-  // Application State
-  const [data, setData] = useState({alfonso: []});
-  let [count, setCount] = useState(0);
-  let [oddCount, setOddCount] = useState(0);
-  const [login, setLogin] = useState(false);
-  const [url, setUrl] = useState();
-
-  // Fetch data whenever url changes
-  useEffect(() => {
-    axios.get("/api/users/").then((result) => setData(result.data));
-  }, []);
-  // Build array of 'alfonso'
-  const alfonsoList = data.alfonso.map((item) => {
-    return (
-      <ul key={item.mammal_id}>
-        <a href={item.url}>{item.title}</a>
-        <li>{item.mammal_name}</li>
-        <li>{item.bio}</li>
-        <li>{item.profile_pic}</li>
-      </ul>
-    );
-  });
-  // const alfonsoList = data.alfonso.map(item => (
-  //   <li key={item.objectID}>
-  //     <a href={item.url}>{item.title}</a>
-  //   </li>
-  // ));
-
-  const countClick = () => {
-    if (oddCount % 2 === 0) {
-      setLogin(true);
-    } else {
-      setLogin(false);
-    }
-    console.log("This is LOGIN ", login);
-    setOddCount((oddCount += 1));
-    // setUrl(BASEURL);
-    // below was original
-    // setUrl(BASEURL + query);
-  };
-
   return (
-    // <Container maxWidth="sm">
-    <div className="App">
-      {<PersistentDrawerRight />}
-      <h3>{title}</h3>
-      <h4>{url}</h4>
-      {/* Use our new Text & Button Component */}
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            {/* <li><Link to="/">Home</Link></li> */}
+            {/* <li><Link to="/mammalprofile">MammalProfile</Link></li> */}
+            {/* <li><Link to="/liveevent">LiveEvent</Link></li> */}
+          </ul>
+        </nav>
 
-      {login && <Homepage />}
-      {/* <Homepage /> */}
-      <Input buttonText="Search" initial="React" onClick={countClick} />
-      <p>You clicked {oddCount} times</p>
-      <ul>{alfonsoList}</ul>
-
-      {/* {login && <AboutUs />} */}
-      <AboutUs />
-      <YouTube />
-      
-    </div>
-    //{" "}
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/mammalprofile">
+            <MammalProfile />
+          </Route>
+          <Route path="/liveevent">
+            <LiveEvent />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
