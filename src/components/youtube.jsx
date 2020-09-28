b7d186b66f6e2aeb96b7b224db517f4766b7a24d
Jordan <jconheasman@gmail.com>
2020-09-27 20:50:05 -0700

    minor formatting changes to youtube component.

--- src/components/youtube.jsx
+++ src/components/youtube.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import YouTube from "react-youtube";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
 
 export default class Example extends React.Component {
   render() {
     const opts = {
-      height: "390",
-      width: "640",
+      height: "253", //390",
+      width: "413", //"640",
+      // device screen spec; W: 414 H: 895.83
+      // can we use style="width:100%;height:auto;"
       playerVars: {
         // https://developers.google.com/youtube/player_parameters
         autoplay: 1,
@@ -13,7 +17,9 @@ export default class Example extends React.Component {
     };
 
     return (
-      <YouTube videoId="WbI1_Z9Cs1A" opts={opts} onReady={this._onReady} />
+      <Container maxWidth="sm">
+        <YouTube videoId="WbI1_Z9Cs1A" opts={opts} onReady={this._onReady} />
+      </Container>
     );
   }
 
