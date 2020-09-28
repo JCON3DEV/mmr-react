ff0fd1ad00fcd939056973785a4bfad1e7527b8b
Serah Sirohi <serahsirohi@gmail.com>
2020-09-28 19:25:56 +0000

    practice react components

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from "react";
 import axios from "axios";
 import Input from "./Input.js";
 import Homepage from "./components/homepage";
 import AboutUs from "./components/about-us";
 import YouTube from "./components/youtube";
-import './App.css';
+import "./App.css";
 
 // Christy's Components
 import PersistentDrawerRight from "./components/nav-drawer";
 
 //import Roboto Font
-import 'fontsource-roboto';
+import "fontsource-roboto";
 
 export default function App() {
   const title = "My Search Page";
-  const BASEURL = '/api/users/';
+  const BASEURL = "/api/users/";
   // above is example for test obj purposes
   //https://hn.algolia.com/api/v1/search?query=';
 
   // Application State
-  const [data, setData] = useState({ alfonso: [] });
+  const [data, setData] = useState({alfonso: []});
   let [count, setCount] = useState(0);
   let [oddCount, setOddCount] = useState(0);
   const [login, setLogin] = useState(false);
   const [url, setUrl] = useState();
-
 
   // Fetch data whenever url changes
   useEffect(() => {
@@ -39,8 +38,8 @@ export default function App() {
         <li>{item.bio}</li>
         <li>{item.profile_pic}</li>
       </ul>
-    )
-    });
+    );
+  });
   // const alfonsoList = data.alfonso.map(item => (
   //   <li key={item.objectID}>
   //     <a href={item.url}>{item.title}</a>
@@ -48,13 +47,13 @@ export default function App() {
   // ));
 
   const countClick = () => {
-    if(oddCount % 2 === 0){
+    if (oddCount % 2 === 0) {
       setLogin(true);
     } else {
       setLogin(false);
-    }  
-    console.log("This is LOGIN ",login);
-    setOddCount(oddCount += 1)
+    }
+    console.log("This is LOGIN ", login);
+    setOddCount((oddCount += 1));
     // setUrl(BASEURL);
     // below was original
     // setUrl(BASEURL + query);
@@ -80,7 +79,4 @@ export default function App() {
     </div>
     //{" "}
   );
-
-
-
 }
