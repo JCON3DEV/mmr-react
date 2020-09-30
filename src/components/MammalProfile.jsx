c0da7317584ebf1a72409d2ff1348d501827e8e6
Jordan <jconheasman@gmail.com>
2020-09-29 22:00:13 -0700

    added a few remaining links

--- src/components/MammalProfile.jsx
+++ src/components/MammalProfile.jsx
@@ -1,18 +1,20 @@
 import { Container } from "@material-ui/core";
 import React from "react";
+
+// Navigation / Routes
+import { Link } from "react-router-dom";
+
 //General Styles/Components
 // import Box from "@material-ui/core/Box";
 // import Container from "@material-ui/core/Container";
-import Donate from "./Donate";
 // Router imports
-import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Button from "@material-ui/core/Button";
 
 
 //Controls components based on maxWidth
 // const useStyles = makeStyles({
 //   root: {
-//     width: "90%",
+//     width: "90%",                                                                                              
 //     maxWidth: 375,
 //   },
 // });
@@ -20,15 +22,13 @@ import Button from "@material-ui/core/Button";
 export default function MammalProfile(props) {
   return (
     // <h3>TESTING Mammel profile page</h3>
-    <Router>
-      {/* <Container className={classes.root}> */}
-        <h1>This is Mammal Profile Blocked</h1>
-        <Route path="/donate" component={Donate}>
-          <Button variant="contained" color="primary" size="large">
-            See All Patients
-          </Button>
-        </Route>
-      {/* </Container> */}
-    </Router>
+    <Container>  
+      <h1>This is Mammal Profile Blocked</h1>
+      <Link className="link" to="/donate">
+        <Button variant="contained" color="primary" size="large">
+          donate
+        </Button>
+      </Link>
+    </Container>
   );
 }
