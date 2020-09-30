4b313aea3dd6714ec4cdcfe4894e672b25cc34eb
Serah Sirohi <serahsirohi@gmail.com>
2020-09-30 20:12:20 +0000

    modifying the about us page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 import "./App.css";
 import "./notifications.css";
+
 import {StaticKitProvider} from "@statickit/react";
 // import axios from "axios";
 
