1000bc289d28ba0769b2d7caf48b66ef715a0bb8
Christy Lum <christy.lum121@gmail.com>
2020-10-02 14:00:34 -0700

    updated nav styles, global primary colour, updated images

--- src/index.js
+++ src/index.js
@@ -4,7 +4,33 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//global theme
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#DD2D25',
+    },
+    secondary: {
+      main: '#F3A83B',
+    },
+  },
+});
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
+  rootElement
+);
+
+// original code
+// ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
