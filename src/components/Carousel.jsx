15dc05cc4ea4118cbeca98a3e9ba738e663f7d30
Serah Sirohi <serahsirohi@gmail.com>
2020-10-06 00:35:12 +0000

    added arrows

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -2,23 +2,34 @@ import React from "react";
 import "../App.css";
 import Container from "@material-ui/core/Container";
 import Box from "@material-ui/core/Box";
+import {makeStyles} from "@material-ui/core/styles";
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 import Slider from "react-slick";
 
+const useStyles = makeStyles({
+  root: {
+    width: "100%",
+    maxWidth: 325,
+  },
+  slider: {
+    color: "green",
+  },
+});
+
 export default function Carousel() {
   return (
     <Container>
-      <Box mt={10} ml={3}>
+      <Box mt={10} ml={3} className={"slider"}>
         <Slider
           className={"slick-active"}
           arrows={true}
           dots={true}
           slidesToShow={1}
           slidesToScroll={1}
-          speed={300}
+          speed={200}
           color={"blue"}
         >
           <img
