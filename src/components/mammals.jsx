fe53a570c4845c7de600750096f14c7aeb3e8cce
Jordan <jconheasman@gmail.com>
2020-10-05 19:28:44 -0700

    added a toggle and conditional logic for unsponsored animals

--- src/components/mammals.jsx
+++ src/components/mammals.jsx
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from "react";
 import axios from "axios";
 import Heart from "./Heart";
+import Unsponsored from "./unsponsored";
 
 // Navigation / Routes
 import {Link, useParams} from "react-router-dom";
@@ -12,6 +13,8 @@ import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
 import Divider from "@material-ui/core/Divider";
 import Button from "@material-ui/core/Button";
+import CheckIcon from "@material-ui/icons/Check";
+import ToggleButton from "@material-ui/lab/ToggleButton";
 import "../App.css";
 
 // Seal Card Elements
@@ -35,21 +38,66 @@ const useStyles = makeStyles({
   }
 });
 
+
 function Mammals() {
   const [mammals, setMammals] = useState([]);
+  let [selected, setSelected] = React.useState(false);
+  const [filtered, setFiltered] = useState([]);
   const classes = useStyles();
+  // Below is coding gold do not delete comment; learn the async --> await functionality
+  const update = async ()=>{
+    let toggle = await Unsponsored().unsponsored;
+    return setFiltered(toggle);
+  }
+  
+  update();
 
   useEffect(() => {
     axios.get("/api/mammals").then((result) => {
       setMammals(result.data.mammals);
     });
   }, []);
+  // console.log("THis is the unsponsored array; ", Unsponsored().unsponsored);
+  console.log("State of the switch is;", selected);
+
+  // function FilterSponsoredMammals(){
+  //   useEffect(() => {
+  //     axios.get("/api/mammals/unsponsored").then((result) => {
+  //       setMammals(result.data.mammals);
+  //     });
+  //   }, []);
+  // }
+
+  const iterate = function(array){
+    array.map((item) =>{
+      console.log(item);
+    })
+  };
 
   return (
     <Container maxWidth="sm">
       <Box mt={12}>
         <Typography variant="h3" gutterBottom align="center">
           Patient Directory
+        </Typography>
+      </Box>
+
+      <Box mt={4}>
+        <Typography variant="h3" gutterBottom align="center">
+          Filter Unsponsored Mammals
+          <ToggleButton
+            value="check"
+            selected={selected}
+            onChange={() => {
+              setSelected(!selected);
+              iterate(filtered);
+            }}
+          >
+            <CheckIcon />
+            {/* {selected === true ? Unsponsored() : setSelected(false)} */}
+            {/* {{if({selected} = true)}} */}
+            {}
+          </ToggleButton>
         </Typography>
       </Box>
 
@@ -73,6 +121,7 @@ function Mammals() {
               <Box mt={4}>
                 <Card>
                   {/* The address seals/${item.id} is set on App.js to the correct view */}
+                  {/* {console.log(item)} */}
                   <Link className="link" to={`/seals/${item.id}`}>
                     <CardActionArea>
                       <CardMedia
@@ -85,14 +134,19 @@ function Mammals() {
                       />
 
                       <CardContent>
-                        <Typography gutterBottom variant="h5" component="h2" align="center" className={classes.textColor}>
+                        <Typography
+                          gutterBottom
+                          variant="h5"
+                          component="h2"
+                          align="center"
+                          className={classes.textColor}
+                        >
                           {item.mammal_name}
                         </Typography>
                       </CardContent>
                     </CardActionArea>
                   </Link>
                 </Card>
-
               </Box>
             </>
           ))}
